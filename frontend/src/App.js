d4976f6c9f0336b16e28153fac55af3351515c42
nottiffchan <nottiffchan@gmail.com>
2022-08-27 23:23:44 +0800

    Add base styles to index.css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,21 @@
-import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
-import SignupPage from './components/SignupPage';
-import {Box} from "@mui/material";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignupPage from "./pages/SignupPage";
+import LandingPage from "./pages/LandingPage";
+import { Box } from "@mui/material";
 
 function App() {
-    return (
-        <div className="App">
-            <Box display={"flex"} flexDirection={"column"} padding={"4rem"}>
-                <Router>
-                    <Routes>
-                        <Route exact path="/" element={<Navigate replace to="/signup" />}></Route>
-                        <Route path="/signup" element={<SignupPage/>}/>
-                    </Routes>
-                </Router>
-            </Box>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Box display={"flex"} flexDirection={"column"} padding={"4rem"}>
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<LandingPage />}></Route>
+            <Route path="/signup" element={<SignupPage />} />
+          </Routes>
+        </Router>
+      </Box>
+    </div>
+  );
 }
 
 export default App;
