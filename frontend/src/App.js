25d1326c90c5c72e71f74035b44ba894edc65074
nottiffchan <nottiffchan@gmail.com>
2022-08-28 00:34:38 +0800

    Basic components, sign up page UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignupPage from "./pages/SignupPage";
 import LandingPage from "./pages/LandingPage";
-import { Box } from "@mui/material";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <div className="App">
-      <Box display={"flex"} flexDirection={"column"} padding={"4rem"}>
-        <Router>
-          <Routes>
-            <Route exact path="/" element={<LandingPage />}></Route>
-            <Route path="/signup" element={<SignupPage />} />
-          </Routes>
-        </Router>
-      </Box>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<LandingPage />}></Route>
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/home" element={<HomePage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- frontend/src/pages/SignupPage.js
+++ frontend/src/pages/SignupPage.js
@@ -1,98 +1,145 @@
-import {
-    Box,
-    Button,
-    Dialog,
-    DialogActions,
-    DialogContent,
-    DialogContentText,
-    DialogTitle,
-    TextField,
-    Typography
-} from "@mui/material";
-import {useState} from "react";
+import styled from "styled-components";
+import { useState } from "react";
 import axios from "axios";
-import {URL_USER_SVC} from "../configs";
-import {STATUS_CODE_CONFLICT, STATUS_CODE_CREATED} from "../constants";
-import {Link} from "react-router-dom";
+import { URL_USER_SVC } from "../configs";
+import { STATUS_CODE_CONFLICT, STATUS_CODE_CREATED } from "../constants";
+import { Link } from "react-router-dom";
+import logo from "../assets/Logo.svg";
+import Button from "../components/Button";
 
 function SignupPage() {
-    const [username, setUsername] = useState("")
-    const [password, setPassword] = useState("")
-    const [isDialogOpen, setIsDialogOpen] = useState(false)
-    const [dialogTitle, setDialogTitle] = useState("")
-    const [dialogMsg, setDialogMsg] = useState("")
-    const [isSignupSuccess, setIsSignupSuccess] = useState(false)
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [isSignupSuccess, setIsSignupSuccess] = useState(false);
 
-    const handleSignup = async () => {
-        setIsSignupSuccess(false)
-        const res = await axios.post(URL_USER_SVC, { username, password })
-            .catch((err) => {
-                if (err.response.status === STATUS_CODE_CONFLICT) {
-                    setErrorDialog('This username already exists')
-                } else {
-                    setErrorDialog('Please try again later')
-                }
-            })
-        if (res && res.status === STATUS_CODE_CREATED) {
-            setSuccessDialog('Account successfully created')
-            setIsSignupSuccess(true)
+  const handleSignup = async () => {
+    setIsSignupSuccess(false);
+    const res = await axios
+      .post(URL_USER_SVC, { username, password })
+      .catch((err) => {
+        if (err.response.status === STATUS_CODE_CONFLICT) {
+          setError("This username already exists");
+        } else {
+          setError("Please try again later");
         }
+      });
+    if (res && res.status === STATUS_CODE_CREATED) {
+      setIsSignupSuccess(true);
     }
+  };
 
-    const closeDialog = () => setIsDialogOpen(false)
-
-    const setSuccessDialog = (msg) => {
-        setIsDialogOpen(true)
-        setDialogTitle('Success')
-        setDialogMsg(msg)
-    }
-
-    const setErrorDialog = (msg) => {
-        setIsDialogOpen(true)
-        setDialogTitle('Error')
-        setDialogMsg(msg)
-    }
-
-    return (
-        <Box display={"flex"} flexDirection={"column"} width={"30%"}>
-            <Typography variant={"h3"} marginBottom={"2rem"}>Sign Up</Typography>
-            <TextField
-                label="Username"
-                variant="standard"
-                value={username}
+  return (
+    <div
+      style={{
+        height: "100%",
+        width: "100vw",
+        overflowX: "hidden",
+      }}
+    >
+      <CardPageWrap>
+        <a href="/">
+          <img
+            src={logo}
+            alt="logo"
+            style={{
+              height: "32px",
+              margin: "60px 0 40px 0",
+              cursor: "pointer",
+            }}
+          />
+        </a>
+        <Header>Welcome to Peerprep!</Header>
+        <CardWrap>
+          <Card>
+            <div style={{ width: "100%", marginBottom: "24px" }}>
+              <label>Username</label>
+              <input
+                type="username"
+                required
+                placeholder="Your username here"
                 onChange={(e) => setUsername(e.target.value)}
-                sx={{marginBottom: "1rem"}}
-                autoFocus
-            />
-            <TextField
-                label="Password"
-                variant="standard"
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                sx={{marginBottom: "2rem"}}
-            />
-            <Box display={"flex"} flexDirection={"row"} justifyContent={"flex-end"}>
-                <Button variant={"outlined"} onClick={handleSignup}>Sign up</Button>
-            </Box>
+                value={username}
+                style={{ width: "356px" }}
+              />
+              {error && (
+                <p style={{ color: "var(--red)", marginTop: "8px" }}>{error}</p>
+              )}
+            </div>
 
-            <Dialog
-                open={isDialogOpen}
-                onClose={closeDialog}
+            <label>Password</label>
+            <input
+              type="password"
+              required
+              placeholder="••••••••"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              style={{ width: "356px" }}
+            />
+            <Button
+              variant="primary"
+              size="big"
+              style={{ marginTop: "48px", width: "100%" }}
+              onClick={handleSignup}
             >
-                <DialogTitle>{dialogTitle}</DialogTitle>
-                <DialogContent>
-                    <DialogContentText>{dialogMsg}</DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    {isSignupSuccess
-                        ? <Button component={Link} to="/login">Log in</Button>
-                        : <Button onClick={closeDialog}>Done</Button>
-                    }
-                </DialogActions>
-            </Dialog>
-        </Box>
-    )
+              Sign up
+            </Button>
+            <p>
+              Already have an account? <a href="/home">Log in</a>
+            </p>
+          </Card>
+        </CardWrap>
+      </CardPageWrap>
+    </div>
+  );
 }
 
 export default SignupPage;
+
+const CardPageWrap = styled.div`
+  width: 100%;
+  max-width: 1200px;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 0 24px;
+  overflow-x: hidden;
+`;
+
+const Header = styled.h1`
+  margin: 0 auto;
+  max-width: 700px;
+  text-align: center;
+  font-size: var(--text-xl);
+  font-weight: 800;
+`;
+
+const CardWrap = styled.div`
+  display: grid;
+  margin: 0;
+  gap: 24px;
+  padding: 24px 0;
+
+  @media (min-width: 640px) {
+    padding: 32px 0;
+  }
+
+  @media (min-width: 832px) {
+    padding: 64px 48px;
+  }
+`;
+
+const Card = styled.div`
+  display: block;
+  padding: 24px;
+  max-width: 400px;
+
+  @media (min-width: 640px) {
+    padding: 32px;
+  }
+
+  @media (min-width: 832px) {
+    padding: 32px 48px;
+    margin: 0 auto;
+    width: 100%;
+  }
+`;
