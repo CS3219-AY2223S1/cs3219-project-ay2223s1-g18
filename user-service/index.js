577077ca476e95efdf020efb3d9b3394961ca860
Theodore Pinto <theopinto98@gmail.com>
2022-08-28 08:45:14 +0800

    Integrate user impln into index

--- user-service/index.js
+++ user-service/index.js
@@ -1,22 +1,43 @@
 import express from 'express';
 import cors from 'cors';
 
-const app = express();
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(cors()) // config cors so that front-end can use
-app.options('*', cors())
+const database = require("./database/connection");
+const users = require("./accounts/routes/users.route");
 import { createUser } from './controller/user-controller.js';
+const port = process.env.USER_PORT || 3000;
+
+const app = express();
+
+app.use(cors()) // config cors so that front-end can use
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.options('*', cors())
 
 const router = express.Router()
 
-// Controller will contain all the User-defined Routes
+app.use("/users", users);
+
+
+// Controller will contain all the User-defined Routes - To be deleted
 router.get('/', (_, res) => res.send('Hello World from user-service'))
 router.post('/', createUser)
 
+app.get("/health-check", (req, res) => {
+    res.json({
+      status: "ok",
+    });
+  });
+  
 app.use('/api/user', router).all((_, res) => {
     res.setHeader('content-type', 'application/json')
     res.setHeader('Access-Control-Allow-Origin', '*')
 })
 
-app.listen(8000, () => console.log('user-service listening on port 8000'));
+try {
+    database.connection();
+  } catch (e) {
+    console.error(e);
+  }
+  
+
+app.listen(port, () => console.log('user-service listening on port:'+ port));
