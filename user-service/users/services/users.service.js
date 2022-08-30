4535d2a3ab1c34ce0919617a08f4ccc2b0d3bb0d
Theodore Pinto <theopinto98@gmail.com>
2022-08-30 10:54:02 +0800

    Remove try catch to enable controllers to handle issue with service and return appropriate code

--- user-service/users/services/users.service.js
+++ user-service/users/services/users.service.js
@@ -6,65 +6,40 @@ import { hashPassword, verifyHashPassword, createJwtToken } from './authenticati
 export default class UserService {
 
   static async createUser(email, username, password) {
-    try {
-      const hashedPassword = await hashPassword(password)
-      console.log(10101)
-      return await Helper.save(UserModel, {
-        username,
-        email,
-        password: hashedPassword
-      })
-    } catch(e) {
-      console.error(e)
-    }
+    const hashedPassword = await hashPassword(password)
+    return await Helper.save(UserModel, {
+      username,
+      email,
+      password: hashedPassword
+    })
   };
 
   static async authenticateUser(username, password) {
-    try {
-      const matchingUser = await Helper.listOne(UserModel, { username })
-      const isEnteredPasswordValid = await verifyHashPassword(password, matchingUser.password)
-      
-      if(isEnteredPasswordValid)
-        return createJwtToken(username)
-      else
-        throw("errr")
-    } catch(e) {
-        console.error(e)
-    }
+    const matchingUser = await Helper.listOne(UserModel, { username })
+    const isEnteredPasswordValid = await verifyHashPassword(password, matchingUser.password)
+
+    if (isEnteredPasswordValid)
+      return createJwtToken(username)
+    else
+      throw ("errr")
   };
 
 
   static async getUserByName(username) {
-    try {
-      return Helper.list(UserModel, {username})
-    } catch(e) {
-        console.error(e)
-    }
+    return Helper.list(UserModel, { username })
   };
 
   static async getUsers() {
-    try {
-      return Helper.list(UserModel, {})
-    } catch(e) {
-        console.error(e)
-    }
+    return Helper.list(UserModel, {})
   };
 
   static async updateUserByName(username, password) {
-    try {
-      const hashedPassword = await hashPassword(password)
-      return Helper.updateOne(UserModel, { username }, { password: hashedPassword }, { new: true })
-    } catch(e) {
-        console.error(e)
-    }
-    
+    const hashedPassword = await hashPassword(password)
+    return Helper.updateOne(UserModel, { username }, { password: hashedPassword }, { new: true })
+
   };
 
   static async deleteUserByName(username) {
-    try {
-      return Helper.deleteOne(UserModel, { username })
-    } catch(e) {
-        console.error(e)
-    }
+    return Helper.deleteOne(UserModel, { username })
   };
 }
