dfeaaf99e5c22096368d61f17ae9614c2e031f65
Theodore Pinto <theopinto98@gmail.com>
2022-08-31 08:55:31 +0800

    Add functionality to analyse jwt function to cross-check redis instance

--- user-service/users/services/authentication.service.js
+++ user-service/users/services/authentication.service.js
@@ -7,33 +7,39 @@ dotenv.config();
 const redisClient = startRedisClient()
 const KEY_VALUE = "valid"
 
-export async function hashPassword(password)  {
+export async function hashPassword(password) {
     return bcrypt.hash(password, parseInt(process.env.HASH_SALT_ROUNDS));
 };
 
-export async function verifyHashPassword(enteredPassword, storedHashPassword)  {
+export async function verifyHashPassword(enteredPassword, storedHashPassword) {
     return bcrypt.compare(enteredPassword, storedHashPassword);
 };
 
-export function createJwtToken(username)  {
+export function createJwtToken(username) {
     return {
         token: jwt.sign(
-            { username }, 
-            process.env.JWT_TOKEN_SECRET, 
+            { username },
+            process.env.JWT_TOKEN_SECRET,
             { expiresIn: process.env.JWT_TOKEN_EXPIRY }),
         expiresIn: process.env.JWT_TOKEN_EXPIRY
     }
 };
 
-export function analyseJwtToken(token) {
+export function analyseJwtToken(token, isBlacklistingToken) {
     if (token == null)
-        throw({ name: 'JsonWebTokenError' });
-    return jwt.verify(token.split(' ')[1], process.env.JWT_TOKEN_SECRET);
+        throw ({ name: 'JsonWebTokenError' });
+    const decodedToken = jwt.verify(token.split(' ')[1], process.env.JWT_TOKEN_SECRET);
+    if (!isBlacklistingToken) {
+        const status = await redisClient.GET(token);
+
+        if (status)
+            throw ({ name: 'JsonWebTokenError' });
+    }
+    return decodedToken;
 }
 
 export function blacklistJwtToken(token) {
-    const tokenData = analyseJwtToken(token);
-    const insertionStatus = await client.SET(token, KEY_VALUE);
-    await client.EXPIREAT(token, +payload.exp);
-
+    const tokenData = analyseJwtToken(token, true);
+    const insertionStatus = await redisClient.SET(token, KEY_VALUE);
+    await redisClient.EXPIREAT(token, +tokenData.exp);
 }
