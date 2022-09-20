ddadd8978f4820d09eee524b78a7afff5aa28cb4
theopin <theopinto98@gmail.com>
2022-09-20 09:09:35 +0800

    Resolve issues with userSignup failure

--- user-service/mailer/process.js
+++ user-service/mailer/process.js
@@ -1,38 +1,38 @@
-
 import nodemailer from 'nodemailer'
 
-async function sendEmail(options) {   
-    const transporter = nodemailer.createTransport({
-      host: process.env.EMAIL_HOST,
-      port: process.env.EMAIL_PORT,
-      secure: true, 
-      auth: {
-        user: process.env.EMAIL_USERNAME,
-        pass: process.env.EMAIL_PASSWORD
-      },
-    });
-  
-    const mailOptions = {
-      from: process.env.EMAIL_FROM,
-      to: options.to,
-      subject: options.subject,
-      html: options.text,
-    };
-  
-    await transporter.sendMail(mailOptions)
-}
 export async function sendValidationEmailRequest(enclosedDetails, message, token, isSignup=false) {
   const clientUrl = "http://localhost:3000"// to be changed later
 
   const apiType = isSignup ? "completeSignup" : "passwordReset"
+  const emailSubject = isSignup ? "Welcome to Peerprep" : "Password Reset Request"
   const urlLinkHeader = isSignup ? "Complete Signup" : "Reset Password"
   
   const resetUrl = `${clientUrl}/${apiType}?token=${token}`
   
   const emailDetails = {
     to: enclosedDetails.email,
-    subject: "Password Reset Request",
+    subject: emailSubject,
     text: message + `<a href=${resetUrl} clicktracking=off>${urlLinkHeader}</a>`,
   }
   await sendEmail(emailDetails);
+}
+
+async function sendEmail(options) {   
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    secure: true, 
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.EMAIL_FROM,
+    to: options.to,
+    subject: options.subject,
+    html: options.text,
+  };
+
+  await transporter.sendMail(mailOptions)
 }
--- user-service/users/services/users.service.js
+++ user-service/users/services/users.service.js
@@ -1,4 +1,3 @@
-
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
