cb98c74bfaf6193019336acc43d1197ec84c34f0
nottiffchan <nottiffchan@gmail.com>
2022-10-08 17:35:52 +0800

    Refactor Difficulty Chip into it's own component

--- frontend/src/components/Interview/Question.js
+++ frontend/src/components/Interview/Question.js
@@ -1,17 +1,7 @@
-import React, { useState, useEffect } from "react";
-import { STATUS_CODE_OK } from "../../utils/constants";
-import { QuestionSvcGETRequest } from "../../utils/QuestionService";
+import React from "react";
+import DifficultyChip from "../DifficultyChip";
 
 const Question = ({ difficulty, question }) => {
-  // const [question, setQuestion] = useState();
-  // useEffect(() => {
-  //   QuestionSvcGETRequest(`/${question_id}`).then((res) => {
-  //     if (res.status == STATUS_CODE_OK) {
-  //       setQuestion(res.data.data);
-  //     }
-  //   });
-  // }, []);
-
   return (
     question && (
       <div
@@ -25,7 +15,9 @@ const Question = ({ difficulty, question }) => {
         <h4>
           <b>{question.title}</b>
         </h4>
-        <DifficultyChip difficulty={difficulty} />
+        <div style={{ marginTop: "8px", marginBottom: "28px" }}>
+          <DifficultyChip difficulty={difficulty} />
+        </div>
         <div dangerouslySetInnerHTML={{ __html: question.content }} />
       </div>
     )
@@ -33,29 +25,3 @@ const Question = ({ difficulty, question }) => {
 };
 
 export default Question;
-
-const DifficultyChip = ({ difficulty }) => {
-  return (
-    <div
-      style={{
-        padding: "4px 8px",
-        borderRadius: "6px",
-        textTransform: "capitalize",
-        fontSize: "12px",
-        color: "white",
-        marginTop: "8px",
-        marginBottom: "28px",
-        width: "min-content",
-        backgroundColor: `${
-          difficulty === "easy"
-            ? "var(--green)"
-            : difficulty === "medium"
-            ? "var(--yellow)"
-            : "var(--red)"
-        }`,
-      }}
-    >
-      {difficulty}
-    </div>
-  );
-};
