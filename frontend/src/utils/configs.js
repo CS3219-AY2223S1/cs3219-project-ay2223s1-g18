53a897ec06ca3f27b3b88a92a9af0fd9b20d5c35
theopin <theopinto98@gmail.com>
2022-10-13 16:46:30 +0800

    Rework frontend to use nginx endpoint

--- frontend/src/utils/configs.js
+++ frontend/src/utils/configs.js
@@ -1,14 +1,12 @@
-const URI_USER_SVC = process.env.REACT_APP_URI_USER_SVC || "http://localhost:8000";
+const API_SVC = process.env.REACT_APP_API_SVC;
 
-const PREFIX_USER_SVC = "/api/user";
 
-export const URL_USER_SVC = URI_USER_SVC + PREFIX_USER_SVC;
+export const URL_USER_SVC = API_SVC ? API_SVC +  "/api/user" : "http://localhost:8000/api/user";
 
-export const SOCKET_URL = process.env.REACT_APP_URI_MATCHING_SVC || "http://localhost:8001/";
+export const SOCKET_URL = API_SVC ? API_SVC +  "/api/matching" : "http://localhost:8001/";
 
-const URI_QUESTION_SVC = process.env.REACT_APP_URI_QUESTION_SVC || "http://localhost:8002";
 
-const PREFIX_QUESTION_SVC = "/api/questions";
+export const URL_QUESTION_SVC = API_SVC ? API_SVC +  "/api/questions" : "http://localhost:8002/api/questions";
 
-export const URL_QUESTION_SVC = URI_QUESTION_SVC + PREFIX_QUESTION_SVC;
+export const URL_USER_HISTORY_SVC = API_SVC ? API_SVC +  "/api/user-history" : "http://localhost:8003/api/user-history";
 
