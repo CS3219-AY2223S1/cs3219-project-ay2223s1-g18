1a14f7513daff3270bb493cac27dc51620e20db3
theopin <theopinto98@gmail.com>
2022-10-14 21:36:26 +0800

    Resolve issues with matching issues

--- frontend/src/utils/configs.js
+++ frontend/src/utils/configs.js
@@ -2,7 +2,7 @@ const API_SVC = process.env.REACT_APP_API_SVC;
 
 export const URL_USER_SVC = API_SVC ? API_SVC +  "/api/user" : "http://localhost:8000/api/user";
 
-export const SOCKET_URL = API_SVC ? API_SVC +  "/api/matching" : "http://localhost:8001/";
+export const SOCKET_URL = API_SVC ? API_SVC  : "http://localhost:8001/";
 
 export const URL_QUESTION_SVC = API_SVC ? API_SVC +  "/api/questions" : "http://localhost:8002/api/questions";
 
