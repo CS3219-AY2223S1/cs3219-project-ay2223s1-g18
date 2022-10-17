a9a431bc9a899174c6098a35e90b9dd8d65b58c8
Zhi-You <tzyou98@gmail.com>
2022-10-17 17:11:00 +0800

    Add get health check for user history and question

--- question-service/question.route.js
+++ question-service/question.route.js
@@ -2,6 +2,8 @@ let router = require("express").Router();
 const QuestionController = require("./question.controller");
 
 router.get("/", QuestionController.getQuestion);
+router.get("/health", QuestionController.getHealthStatus);
+
 router.get("/:question_id", QuestionController.getQuestionById);
 
 module.exports = router;
