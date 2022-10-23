9fbe1fceb71fe13315c175c42ff72452705f1667
theopin <theopinto98@gmail.com>
2022-10-23 13:40:01 +0800

    Refine tests for gateway match

--- gateway-service/tests/scenarios/runMatchingTests.js
+++ gateway-service/tests/scenarios/runMatchingTests.js
@@ -1,51 +1,59 @@
-import { createServer } from "http";
 import { io as Client } from "socket.io-client";
-import { Server } from "socket.io";
-import { assert } from "chai";
-import dotenv from 'dotenv';
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
 
-const GATEWAY_LINK = process.env.GATEWAY_LINK
-const ACCESS_TOKEN = process.env.ACCESS_TOKEN
+//const GATEWAY_LINK = process.env.GATEWAY_LINK;
+const GATEWAY_LINK = 'http://localhost:8001'
+//const ACCESS_TOKEN = process.env.ACCESS_TOKEN
 
 export function runMatchingTests() {
   describe("Matching Tests /", () => {
     let io, serverSocket, clientSocket;
+    let client1 = new Client(GATEWAY_LINK);
+    let client2 = new Client(GATEWAY_LINK);
+    let client3 = new Client(GATEWAY_LINK);
 
-    //TODO: Change client to api gateway
-    before((done) => {
-      const httpServer = createServer();
-      io = new Server(httpServer);
-      httpServer.listen(() => {
-        const port = httpServer.address().port;
-        clientSocket = new Client(`http://localhost:${port}`);
-        io.on("connection", (socket) => {
-          serverSocket = socket;
-        });
-        clientSocket.on("connect", done);
+    it("should render sockets for clients", (done) => {
+      client1.emit("render", "hello");
+      client2.emit("render", "hello");
+      client3.emit("render", "hello");
+      setTimeout(() => {
+        done();
+      }, 1000);
+    });
+
+    it("should not have a room for a client", (done) => {
+      client1.emit("check room existence");
+      client1.on("does room exist", (isTrue) => {
+        if (!isTrue) {
+          done();
+        }
       });
     });
 
-    after(() => {
-      io.close();
-      clientSocket.close();
-    });
-
-    it("should work", (done) => {
-      clientSocket.on("hello", (arg) => {
-        assert.equal(arg, "world");
+    it("should initiate match for a client", (done) => {
+      client1.emit("match request", "client1", "hard", 123, client1.id);
+      client1.on("initiate match", () => {
         done();
       });
-      serverSocket.emit("hello", "world");
     });
 
-    it("should work (with ack)", (done) => {
-      serverSocket.on("hi", (cb) => {
-        cb("hola");
-      });
-      clientSocket.emit("hi", (arg) => {
-        assert.equal(arg, "hola");
+    it("should find a match for a client", (done) => {
+      setTimeout(() => {
+        client2.emit("match request", "client2", "hard", 123, client2.id);
+      }, 1000);
+
+      client1.on("successfulMatch", () => {
         done();
+      });
+    });
+
+    it("should have a room for a client", (done) => {
+      client1.emit("check room existence");
+      client1.on("does room exist", (isTrue) => {
+        if (isTrue) {
+          done();
+        }
       });
     });
   });
