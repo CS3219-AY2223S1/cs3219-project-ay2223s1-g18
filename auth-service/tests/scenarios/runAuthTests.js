df02fcf706e08c96b179953920942ca81cb6b237
theopin <theopinto98@gmail.com>
2022-10-26 19:34:16 +0800

    Add testing for auth tests

--- auth-service/tests/scenarios/runAuthTests.js
+++ auth-service/tests/scenarios/runAuthTests.js
@@ -1,42 +1,48 @@
-import chai from 'chai'
-import dotenv from 'dotenv'
-dotenv.config()
+import chai from "chai";
+import dotenv from "dotenv";
+dotenv.config();
 
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
+const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
+const VERIFICATION_TOKEN = process.env.VERIFICATION_TOKEN;
 
 export function runAuthTests(app) {
   describe("Auth /", () => {
-
-    it("should be able to access list of accounts", (done) => {
-      chai.request(app)
-        .get(`/initialize-tokens`)
+    it("should logout", (done) => {
+      chai
+        .request(app)
+        .post(`/api/auth/logout`)
+        .set({ Authorization: `Bearer ${REFRESH_TOKEN}` })
         .end((err, res) => {
-          res.should.have.status(200)
-          res.body.should.be.a('object')
-          done()
-        })
-    })
+          res.should.have.status(200);
+          res.body.should.be.a("object");
+          done();
+        });
+    });
 
-    // it("should not be perform verification request without a valid verification token", (done) => {
-    //   chai.request(USER_SERVICE_LINK)
-    //     .patch(`/api/user/password-reset-verify`)
-    //     .end((err, res) => {
-    //       res.should.have.status(401)
-    //       res.body.should.be.a('object')
-    //       done()
-    //     })
-    // })
+    it("should not logout again", (done) => {
+      chai
+        .request(app)
+        .post(`/api/auth/logout`)
+        .set({ Authorization: `Bearer ${REFRESH_TOKEN}` })
+        .end((err, res) => {
+          res.should.have.status(401);
+          res.body.should.be.a("object");
+          done();
+        });
+    });
 
-    // it("should authenticate a user", (done) => {
-    //   chai.request(USER_SERVICE_LINK)
-    //     .post(`/api/user/auth`)
-    //     .type('form')
-    //     .send({username: 'hong', password: 'test'})
-    //     .end((err, res) => {
-    //       res.should.have.status(200)
-    //       res.body.should.be.a('object')
-    //       done()
-    //     })
-    // })
+    it("should verify access tokens", (done) => {
+      chai
+        .request(app)
+        .post(`/api/auth/access`)
+        .set({ Authorization: `Bearer ${ACCESS_TOKEN}` })
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a("object");
+          done();
+        });
+    });
 
-  })
+  });
 }
