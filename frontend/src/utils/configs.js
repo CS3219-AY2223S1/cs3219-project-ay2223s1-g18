85bfd8c4807792d42fd47b769ff89b06bad5fd3a
nottiffchan <nottiffchan@gmail.com>
2022-10-29 00:06:11 +0800

    Figuring out gateway and docker stuff

--- frontend/src/utils/configs.js
+++ frontend/src/utils/configs.js
@@ -1,28 +1,26 @@
 const API_SVC = process.env.REACT_APP_API_SVC;
-const isUsingGateway = process.env.REACT_APP_IS_USING_GATEWAY === 'true';
+const isUsingGateway = process.env.REACT_APP_IS_USING_GATEWAY === "true";
 
+export const URL_USER_SVC = isUsingGateway
+  ? API_SVC + "/api/user"
+  : process.env.REACT_APP_URI_USER_SVC
+  ? process.env.REACT_APP_URI_USER_SVC
+  : "http://localhost:8000/";
 
-export const URL_USER_SVC = isUsingGateway 
-    ? API_SVC +  "/api/user" 
-    : process.env.REACT_APP_URI_USER_SVC 
-    ? process.env.REACT_APP_URI_USER_SVC 
-    : "http://localhost:8000/api/user";
+export const SOCKET_URL = isUsingGateway
+  ? API_SVC
+  : process.env.REACT_APP_URI_MATCHING_SVC
+  ? process.env.REACT_APP_URI_MATCHING_SVC
+  : "http://localhost:8001/";
 
-export const SOCKET_URL = isUsingGateway 
-    ? API_SVC  
-    : process.env.REACT_APP_URI_MATCHING_SVC 
-    ? process.env.REACT_APP_URI_MATCHING_SVC 
-    : "http://localhost:8001/";
+export const URL_QUESTION_SVC = isUsingGateway
+  ? API_SVC + "/api/questions"
+  : process.env.REACT_APP_URI_QUESTION_SVC
+  ? process.env.REACT_APP_URI_QUESTION_SVC
+  : "http://localhost:8002/";
 
-export const URL_QUESTION_SVC = isUsingGateway 
-    ? API_SVC +  "/api/questions" 
-    : process.env.REACT_APP_URI_QUESTION_SVC 
-    ? process.env.REACT_APP_URI_QUESTION_SVC 
-    : "http://localhost:8002/api/questions";
-
-export const URL_USER_HISTORY_SVC = isUsingGateway 
-    ? API_SVC +  "/api/user-history" 
-    : process.env.REACT_APP_URI_USER_HISTORY_SVC 
-    ? process.env.REACT_APP_URI_USER_HISTORY_SVC 
-    : "http://localhost:8003/api/user-history";
-
+export const URL_USER_HISTORY_SVC = isUsingGateway
+  ? API_SVC + "/api/user-history"
+  : process.env.REACT_APP_URI_USER_HISTORY_SVC
+  ? process.env.REACT_APP_URI_USER_HISTORY_SVC
+  : "http://localhost:8003/";
